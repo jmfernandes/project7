39e8c5a7294a82d09554623313ed671fe1c0fc8d
jmfernandes <joshfernandes@mac.com>
2017-08-19 12:28:23 -0700

    changed style to remove scope

--- scripts/controllers/mainController.js
+++ scripts/controllers/mainController.js
@@ -1,4 +1,13 @@
-mainApp.controller('viewController', ['$scope', function($scope){
+var viewController = function($scope){
   var mainCtrl = this;
   mainCtrl.message = 'hello there, this is the main message';
-}]);
+  $scope.message2 = 'second message';
+};
+
+viewController.$inject = ['$scope']; //this needed instead of doing
+// mainApp.controller = ('viewController',['$scope',function($scope){}]);
+//the above line is in line dependency injection, but if you do the new
+//style where you define a varable as the controller then you need
+//to inject so that it works while minify
+
+mainApp.controller('viewName',viewController);
