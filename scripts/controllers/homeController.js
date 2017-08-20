08ca3762f8aace2238c3a0598530d8a9d0e654f4
jmfernandes <joshfernandes@mac.com>
2017-08-20 15:48:01 -0700

    got rid of scope. will no longer be used

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,4 +1,4 @@
-mainApp.controller('HomeController', ['$scope', function($scope){
+mainApp.controller('HomeController', [function(){
   var homeCtrl = this;
   homeCtrl.message = 'This is the home page';
   homeCtrl.math = _.add(6, 4);
--- scripts/controllers/mainController.js
+++ scripts/controllers/mainController.js
@@ -1,13 +1,12 @@
-var viewController = function($scope){
+mainApp.controller('viewController', [function(){
   var mainCtrl = this;
   mainCtrl.message = 'hello there, this is the main message';
-  $scope.message2 = 'second message';
-};
+}]);
 
-viewController.$inject = ['$scope']; //this needed instead of doing
+//viewController.$inject = ['$scope']; //this needed instead of doing
 // mainApp.controller = ('viewController',['$scope',function($scope){}]);
 //the above line is in line dependency injection, but if you do the new
 //style where you define a varable as the controller then you need
 //to inject so that it works while minify
 
-mainApp.controller('viewName',viewController);
+//mainApp.controller('viewName',viewController);
