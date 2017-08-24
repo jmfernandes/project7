b3167d1b2f2e561781465c32ae737f65c945468a
jmfernandes <joshfernandes@mac.com>
2017-08-24 13:31:31 -0700

    Added last example

--- scripts/controllers/ex5Controller.js
+++ scripts/controllers/ex5Controller.js
@@ -4,4 +4,17 @@ mainApp.controller('ex5Controller', [function(){
     console.log('user submitted with ',
         self.user);
   };
+  self.sports = [
+    {label: 'Basketball',selected: 'YES'},
+    {label: 'Cricket',selected: 'NO'},
+    {label: 'Soccer',selected: 'NO'},
+    {label: 'Swimming',selected: 'YES'}
+  ];
+  self.countries = [
+    {label:'USA',id:1},
+    {label:'India',id:2},
+    {label:'Other',id:3}
+  ];
+  self.selectedCountryId = 2;
+  self.selectedCountry = self.countries[1];
 }]);
